17995afd97aa3621bcbc2cf5b6d7135001342a6e
craftsmanWX <weixu.craftsman@gmail.com>
2024-07-04 16:16:46 +1000

    implements fetching data from API and insert to DB (#4)

--- src/db_operation/insertOrders.js
+++ src/db_operation/insertOrders.js
@@ -1,10 +1,15 @@
 // insertOrders.js
 import payload from 'payload';
 
-const insertOrders = async () => {
+const insertOrders = async (id) => {
   try {
+    const orderId = Number(id);
+    if (isNaN(orderId)) {
+      throw new Error('Invalid id: must be a number');
+    }
+    
     const newOrder = {
-      total: 110.0, // Replace with the actual total for the order
+      total: orderId, // Replace with the actual total for the order
       // Add more fields as needed for your order
     };
 
--- src/scheduler/scheduler.ts
+++ src/scheduler/scheduler.ts
@@ -1,9 +1,12 @@
 // npm install node-schedule
 const schedule = require('node-schedule');
+import { fetchPosts } from '../fetch_data/fetchData';
+import insertOrders from '../db_operation/insertOrders';
 
 function scheduleTask() {
   let job = schedule.scheduleJob('10 * * * * *', () => {
     console.log(new Date());
+    const id = fetchPosts(insertOrders);
   });
 
   return job;
