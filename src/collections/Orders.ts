aaa1714eecf4b70610b492d0166d31906ae807d6
weixu <weixu.craftsman@gmail.com>
2024-07-10 15:16:42 +1000

    insert raw Json into MongoDB

--- src/collections/Orders.ts
+++ src/collections/Orders.ts
@@ -13,14 +13,26 @@ export const Orders: CollectionConfig = {
     fields: [
         {
             name: 'total',
-            type: 'number',
+            type: 'json',
             required: true,
-        }
+        },
+        // {
+        //     name: 'owner', // required
+        //     type: 'relationship', // required
+        //     relationTo: 'users', // required
+        //     hasMany: false,
+        //   },
+        // {
+        //     name: 'order_time',
+        //     type: 'date',
+        //     required: true,
+        // }
     ],
     access: {
-        read: () => {
-            return true;
-        },
+        read: ({ req }: Context) => {
+            //console.log(req);
+            return !!req.user; 
+          },
         create: () => {
             return true;
         },
--- src/scheduler/scheduler.ts
+++ src/scheduler/scheduler.ts
@@ -2,11 +2,12 @@
 const schedule = require('node-schedule');
 import { fetchPosts } from '../fetch_data/fetchData';
 import insertOrders from '../db_operation/insertOrders';
+import insertRawJson from '../db_operation/insertRawJson';
 
 function scheduleTask() {
   let job = schedule.scheduleJob('10 * * * * *', () => {
     console.log(new Date());
-    const id = fetchPosts(insertOrders);
+    const id = fetchPosts(insertRawJson);
   });
 
   return job;
